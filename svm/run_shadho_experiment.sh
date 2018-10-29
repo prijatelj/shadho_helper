40b3e244c4ba753d24c9735a210aa443a4bd6f9a
prijatelj <theomnipotentwoof@gmail.com>
2018-10-29 13:26:53 -0400

    moved run_shadho_Experiment because python driver was not loking in correct relative dir when run via this script.

--- svm/run_shadho_experiment.sh
+++ svm/run_shadho_experiment.sh
@@ -51,6 +51,7 @@ fi
 
 # Construct Master Name from parts
 MASTER_NAME="$ID-$SCHEDULER-$DATE"
+echo "master_name = $MASTER_NAME"
 
 # Create the shadho worker factories to run in the background.
 #TODO allow user to specif -w and -W in this script
@@ -60,7 +61,7 @@ MASTER_NAME="$ID-$SCHEDULER-$DATE"
 ~/.local/bin/shadho_wq_factory -M $MASTER_NAME -T condor -w 10 -W 20 --cores=16 &
 
 # Run the python driver
-python3 svm/driver.py -o "$OUTPUT_DIR/$MASTER_NAME.json" "$MASTER_NAME"
+python3 driver.py -o "$OUTPUT_DIR/$MASTER_NAME.json" "$MASTER_NAME"
 
 # kill all factories and workers.
 ps aux | grep $1 | grep work_queue | awk '{print $2}' | xargs -L1 kill
